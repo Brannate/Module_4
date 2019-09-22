29dfc8a39d5dcb174dd8347bc50ccf705cbbe5da
Brannate <54637185+Brannate@users.noreply.github.com>
2019-09-22 12:42:55 -0500

    Update if_elif.py

--- if_elif_statement_assignment/if_elif.py
+++ if_elif_statement_assignment/if_elif.py
@@ -11,22 +11,22 @@ trial_cost = 0
 
 # Print statements for subscriptions and costs
 print("Welcome to the Programmer's Toolkit Subscription, the subscription levels are as follows:")
-print("Platinum = $" + str(platinum_cost) + ", Gold = $" + str(gold_cost) + ", Silver = $" + str(gold_cost) + ", Bronze = $" + str(bronze_cost) + ", Trial = $" + str(trial_cost) + ".")
+print("Platinum = $" + str(platinum_cost) + ", Gold = $" + str(gold_cost) + ", Silver = $" + str(silver_cost) + ", Bronze = $" + str(bronze_cost) + ", Trial = $" + str(trial_cost) + ".")
 
 # User input for which subscription level they would like
-subscription_level = input("Please select which level you would like to subscribe to: ")
+subscription_level = str(input("Please select which level you would like to subscribe to: "))
 
 # If statement to print out what level they chose and the cost
-if subscription_level == "Platinum" or "platinum":
-    print("Congratulations, you have signed up for the Platinum subscription. Your cost today is $" + platinum_cost + ".")
-elif subscription_level == "Gold" or "gold":
-    print("Congratulations, you have signed up for the Gold subscription. Your cost today is $" + gold_cost + ".")
-elif subscription_level == "Silver" or "silver":
-    print("Congratulations, you have signed up for the Silver subscription. Your cost today is $" + silver_cost + ".")
-elif subscription_level == "Bronze" or "bronze":
-    print("Congratulations, you have signed up for the Bronze subscription. Your cost today is $" + bronze_cost + ".")
-elif subscription_level == "Trial" or "bronze":
-    print("Congratulations, you have signed up for the free trial. Your cost today is $" + trial_cost + ".")
+if subscription_level == ("Platinum" or "platinum"):
+    print("Congratulations, you signed up for the Platinum subscription. Your cost is $" + str(platinum_cost) + ".")
+elif subscription_level == ("Gold" or "gold"):
+    print("Congratulations, you signed up for the Gold subscription. Your cost is $" + str(gold_cost) + ".")
+elif subscription_level == ("Silver" or "silver"):
+    print("Congratulations, you signed up for the Silver subscription. Your cost is $" + str(silver_cost) + ".")
+elif subscription_level == ("Bronze" or "bronze"):
+    print("Congratulations, you signed up for the Bronze subscription. Your cost is $" + str(bronze_cost) + ".")
+elif subscription_level == ("Trial" or "trial"):
+    print("Congratulations, you signed up for the free trial. Your cost is $" + str(trial_cost) + ".")
 else:
     # Error handling
     print("Invalid input, please try again.")
