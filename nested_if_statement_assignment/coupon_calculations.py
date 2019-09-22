c7e94668140b0881938a90e142e9a4b3e4f8b39b
Brannate <54637185+Brannate@users.noreply.github.com>
2019-09-22 16:32:05 -0500

    Updates

--- nested_if_statement_assignment/coupon_calculations.py
+++ nested_if_statement_assignment/coupon_calculations.py
@@ -14,20 +14,37 @@ def calculate_price(price, cash_coupon, percent_coupon):
     shipping_30to50 = 11.95
     shipping_50up = 0
 
-    # Calculation of price
-    if price 
+    # Calculation of price after coupons
+    # NEED IF STATEMENT FOR COUPONS - LOGIC ERROR
+    price_after_c = (float(price) - float(cash_coupon)) * (float(percent_coupon) / float(100))
 
+    # Calculation of price after shipping
+    if float(price_after_c) < float(10.00):
+        price_after_s = float(price_after_c) + float(shipping_10)
+    elif float(10.00) <= float(price_after_c) < float(30.00):
+        price_after_s = float(price_after_c) + float(shipping_10to30)
+    elif float(30.00) <= float(price_after_c) < float(50.00):
+        price_after_s = float(price_after_c) + float(shipping_30to50)
+    elif float(50.00) < float(price_after_c):
+        price_after_s = float(price_after_c) + float(shipping_50up)
+    else:
+        return "Please enter a positive number for price."
 
-    # Return total price to whatever called the function
+    # Calculation of price after tax
+    total_price = float(price_after_s) * float(tax)
+
+    # Return total price
     return total_price
     # pass
 
 
 def main():
-    purchase_amount = input()
-    cash_c = input()
-    percent_c = input()
+    purchase_amount = input("Please enter the initial purchase price: ")
+    cash_c = input("Please enter any cash coupons: ")
+    percent_c = input("Please enter any percent coupons: ")
     price = calculate_price(purchase_amount, cash_c, percent_c)
+
+    print(str(price))
 
     # pass
 
--- nested_if_statement_assignment/coupon_calculations_test.py
+++ nested_if_statement_assignment/coupon_calculations_test.py
@@ -1,10 +1,39 @@
 import unittest
-from Module_4 import nested_if_statement_assignment as nif
+from Module_4.nested_if_statement_assignment import coupon_calculations
 
 
 class MyTestCase(unittest.TestCase):
-    def test_nif(self):
-        self.assertEqual(True, True)
+    def test_under10(self):
+        self.assertEqual(coupon_calculations.calculate_price(0, 0, 0), 0)
+        self.assertEqual(coupon_calculations.calculate_price(0, 0, 0), 0)
+        self.assertEqual(coupon_calculations.calculate_price(0, 0, 0), 0)
+        self.assertEqual(coupon_calculations.calculate_price(0, 0, 0), 0)
+        self.assertEqual(coupon_calculations.calculate_price(0, 0, 0), 0)
+        self.assertEqual(coupon_calculations.calculate_price(0, 0, 0), 0)
+
+    def test_10toLT30(self):
+        self.assertEqual(coupon_calculations.calculate_price(0, 0, 0), 0)
+        self.assertEqual(coupon_calculations.calculate_price(0, 0, 0), 0)
+        self.assertEqual(coupon_calculations.calculate_price(0, 0, 0), 0)
+        self.assertEqual(coupon_calculations.calculate_price(0, 0, 0), 0)
+        self.assertEqual(coupon_calculations.calculate_price(0, 0, 0), 0)
+        self.assertEqual(coupon_calculations.calculate_price(0, 0, 0), 0)
+
+    def test_30toLT50(self):
+        self.assertEqual(coupon_calculations.calculate_price(0, 0, 0), 0)
+        self.assertEqual(coupon_calculations.calculate_price(0, 0, 0), 0)
+        self.assertEqual(coupon_calculations.calculate_price(0, 0, 0), 0)
+        self.assertEqual(coupon_calculations.calculate_price(0, 0, 0), 0)
+        self.assertEqual(coupon_calculations.calculate_price(0, 0, 0), 0)
+        self.assertEqual(coupon_calculations.calculate_price(0, 0, 0), 0)
+
+    def test_50andO(self):
+        self.assertEqual(coupon_calculations.calculate_price(0, 0, 0), 0)
+        self.assertEqual(coupon_calculations.calculate_price(0, 0, 0), 0)
+        self.assertEqual(coupon_calculations.calculate_price(0, 0, 0), 0)
+        self.assertEqual(coupon_calculations.calculate_price(0, 0, 0), 0)
+        self.assertEqual(coupon_calculations.calculate_price(0, 0, 0), 0)
+        self.assertEqual(coupon_calculations.calculate_price(0, 0, 0), 0)
 
 
 if __name__ == '__main__':
