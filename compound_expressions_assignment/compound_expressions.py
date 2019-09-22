120675d631904dbf8af15fc1fb609330dcac31bd
Brannate <54637185+Brannate@users.noreply.github.com>
2019-09-22 13:10:05 -0500

    working y

--- compound_expressions_assignment/compound_expressions.py
+++ compound_expressions_assignment/compound_expressions.py
@@ -8,13 +8,13 @@ max_value = 10
 min_value = 0
 
 # User input for variable y
-y = input("Please enter a number above " + str(max_value) + " or below " + str(min_value) + ":")
+y = input("Please enter a number above " + str(max_value) + " or below " + str(min_value) + ": ")
 
 # If statement to test y for max and min values
-if int(max_value) < int(y) < int(min_value):
-    print("Sorry, " + str(y) + " is between " + str(max_value) + " and " + str(min_value) + ".")
+if (int(max_value) < int(y)) or (int(y) <= int(min_value)):
+    print("Excellent, " + str(y) + " is not between or equal to " + str(max_value) + " and " + str(min_value) + ".")
 else:
-    print("Excellent, " + str(y) + " is not between " + str(max_value) + " and " + str(min_value) + ".")
+    print("Sorry, " + str(y) + " is between or equal to " + str(max_value) + " and " + str(min_value) + ".")
 
 # User input for variable x
-# x = input()
+x = input()
