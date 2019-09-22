00e8302a2e0047c9f47740d61feab9a82d3191fa
Brannate <54637185+Brannate@users.noreply.github.com>
2019-09-22 15:01:27 -0500

    Update coupon_calculations.py

--- nested_if_statement_assignment/coupon_calculations.py
+++ nested_if_statement_assignment/coupon_calculations.py
@@ -5,23 +5,31 @@ This program is for creating order pricing."""
 
 
 def calculate_price(price, cash_coupon, percent_coupon):
-    # TAX = 1.06
-    # shipping_10 = 5.95
-    # shipping_10to30 = 7.69
-    # shipping_30to50 = 11.95
-    # shipping_50up = 0
+    # Tax variables
+    tax = 1.06
 
-    # total_price =
-    # return total_price
-    pass
+    # Shipping variables
+    shipping_10 = 5.95
+    shipping_10to30 = 7.69
+    shipping_30to50 = 11.95
+    shipping_50up = 0
+
+    # Calculation of price
+    if price 
+
+
+    # Return total price to whatever called the function
+    return total_price
+    # pass
 
 
 def main():
     purchase_amount = input()
     cash_c = input()
     percent_c = input()
-    calculate_price(purchase_amount, cash_c, percent_c)
-    pass
+    price = calculate_price(purchase_amount, cash_c, percent_c)
+
+    # pass
 
 
 if __name__ == '__main__':
