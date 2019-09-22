4b59d76efc26bf9753c40025335ebbf5aaa248e3
Brannate <54637185+Brannate@users.noreply.github.com>
2019-09-22 14:48:14 -0500

    defined functions

---
+++ nested_if_statement_assignment/nested_if.py
@@ -0,0 +1,16 @@
+"""docstring
+This program is for creating order pricing."""
+
+# Start
+
+
+def calculate_price(price, cash_coupon, percent_coupon):
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
