e9a5ce15aaa32af9aac09dc64c20bf8c4ba75d5f
Brannate <54637185+Brannate@users.noreply.github.com>
2019-09-22 14:25:39 -0500

    recreated files and created unit test

---
+++ nested_if_statement_assignment/nested_if_test.py
@@ -0,0 +1,11 @@
+import unittest
+from Module_4 import nested_if_statement_assignment as nif
+
+
+class MyTestCase(unittest.TestCase):
+    def test_nif(self):
+        self.assertEqual(True, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
