f479313c096ca4032fbcf4e65a38701ad1659a82
Brannate <54637185+Brannate@users.noreply.github.com>
2019-09-22 14:56:01 -0500

    renamed files to match assignment

---
+++ nested_if_statement_assignment/coupon_calculations.py
@@ -0,0 +1,28 @@
+"""docstring
+This program is for creating order pricing."""
+
+# Start
+
+
+def calculate_price(price, cash_coupon, percent_coupon):
+    # TAX = 1.06
+    # shipping_10 = 5.95
+    # shipping_10to30 = 7.69
+    # shipping_30to50 = 11.95
+    # shipping_50up = 0
+
+    # total_price =
+    # return total_price
+    pass
+
+
+def main():
+    purchase_amount = input()
+    cash_c = input()
+    percent_c = input()
+    calculate_price(purchase_amount, cash_c, percent_c)
+    pass
+
+
+if __name__ == '__main__':
+    main()
